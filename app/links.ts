ac9db43939652bf799a05387a20308c166dd2a60
oliviaschwob <50679579+oliviaschwob@users.noreply.github.com>
2024-05-06 09:14:05 -0400

    Update links.ts

--- app/links.ts
+++ app/links.ts
@@ -6,8 +6,7 @@ export const links = [
   { name: "Recommendations", href: "/recommendations" },
   { name: "Methodology", href: "/methodology" },
   {
-    name: "Download Report",
-    href: "/public/05022024_RedLinesReport_Final.pdf",
-    download: true,
+    name: "PDF",
+    href: "https://drive.google.com/file/d/10fUJL7dpmWIYyT-LvzAu-YF-7xb_z6Vd/view?usp=drive_link",
   },
 ]
