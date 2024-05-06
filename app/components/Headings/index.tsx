be46150b3be983b4c86293a599ce869758356e80
Ella Heron <heronsound95@gmail.com>
2024-05-05 22:31:34 -0400

    yes

---
+++ app/components/Headings/index.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import classNames from "classnames"
+import { unica } from "../../fonts"
+import Image from "next/image"
+import share from "@/public/images/share.svg"
+import styles from "./Headings.module.css"
+import { usePathname } from "next/navigation"
+import { useState } from "react"
+
+const BASE_URL = "https://redlines.nwu.org"
+
+interface Props extends React.PropsWithChildren {
+  className?: string
+  id?: string
+}
+
+export const Heading: React.FC<Props> = ({ className, children }) => (
+  <h1 className={classNames(className, unica.className)}>{children}</h1>
+)
+
+export const Subheading: React.FC<Props> = ({ className, children, id }) => {
+  const path = usePathname()
+  const [copied, setCopied] = useState(false)
+
+  return (
+    <div className={styles.subheadingContainer}>
+      {/* <button
+        className={classNames(styles.shareButton)}
+        onClick={() => {
+          navigator.clipboard.writeText(
+            `${BASE_URL}${path}${id ? `#${id}` : ""}`
+          )
+          setCopied(true)
+          setTimeout(() => setCopied(false), 5000)
+        }}
+      >
+        <Image
+          src={share}
+          alt="share"
+          priority
+          height={20}
+          width={20}
+          className={classNames(styles.share, copied && styles.copied)}
+        />
+      </button> */}
+      <h3 className={classNames(className, unica.className)} id={id}>
+        {children}
+      </h3>
+    </div>
+  )
+}
